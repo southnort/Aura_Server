27c0e4ceda1a0876fb3783408383fd044ddd4f87
home_machine <home@email.com>
2018-07-24 14:14:54 +0300

    //24.07.2018 * Метод подключения к БД готов

--- Aura_Server/Controller/DataBaseManager.cs
+++ Aura_Server/Controller/DataBaseManager.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SQLite;
+using System.IO;
 
 namespace Aura_Server.Controller
 {
@@ -11,7 +12,27 @@ namespace Aura_Server.Controller
     {
         //менеджер, осуществляющий связь между БД и программой
         //один менеджер на одну базу
+        private SQLiteConnection connection;
+        private SQLiteCommand command;
 
+        public void ConnectToDataBase(string dbFileName)
+        {
+            //подключиться к существующей базе
+            if (!File.Exists(dbFileName))
+            {
+                throw new Exception("DataBase \"" + dbFileName + "\" not found");
+            }
+
+            else
+            {
+                connection = new SQLiteConnection("Data Source=" + dbFileName + ";Version=3;");
+                connection.Open();
+                command = new SQLiteCommand();
+                command.Connection = connection;
+
+            }
+
+        }
 
 
     }
