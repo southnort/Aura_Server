3feab9a22151ad78a5d2e5274ec5721fc3970a02
home_machine <home@email.com>
2018-08-20 02:11:57 +0300

    20.08.2018 * перенос заказчиков из екселя в бд

---
+++ Aura_Server/Excel/ExcelReader.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+
+namespace Aura_Server.Excel
+{
+    public class ExcelReader
+    {
+        //класс предназначен для чтения данных из таблицы Excel
+        public List<List<string>> GetFromFile(string filePath)
+        {
+            List<List<string>> tableCells = new List<List<string>>();
+            if (File.Exists(filePath))
+            {
+                using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                {
+                    using (SpreadsheetDocument doc = SpreadsheetDocument.Open(fs, false))
+                    {
+                        WorkbookPart workbookPart = doc.WorkbookPart;
+                        SharedStringTablePart sstPart = workbookPart.GetPartsOfType<SharedStringTablePart>().First();
+                        SharedStringTable sst = sstPart.SharedStringTable;
+
+                        WorksheetPart worksheetPart = workbookPart.WorksheetParts.Last();
+                        Worksheet sheet = worksheetPart.Worksheet;
+
+                        var cells = sheet.Descendants<Cell>();
+                        var rows = sheet.Descendants<Row>();
+
+
+                        foreach (Row row in rows)
+                        {
+                            List<string> rowList = new List<string>();
+                            foreach (Cell cell in row.Elements<Cell>())
+                            {
+                                if ((cell.DataType != null) && (cell.DataType == CellValues.SharedString))
+                                {
+                                    int ssid = int.Parse(cell.CellValue.Text);
+                                    string str = sst.ChildElements[ssid].InnerText;
+                                    rowList.Add(str);
+                                }
+                                else if (cell.CellValue != null)
+                                {
+
+                                }
+
+                            }
+
+                            tableCells.Add(rowList);
+
+                        }
+
+
+
+                    }
+                }
+
+                return tableCells;
+
+            }
+
+            else
+            {
+                throw new Exception("Файл не найден " + filePath);
+            }
+
+        }
+
+    }
+
+}
--- Aura_Server/Program.cs
+++ Aura_Server/Program.cs
@@ -11,6 +11,9 @@ using Aura_Server.Controller.Network;
 using System.Threading;
 using Aura_Server.Excel;
 
+using Aura.Model;
+using System.Text.RegularExpressions;
+
 namespace Aura_Server
 {
     class Program
@@ -91,74 +94,334 @@ namespace Aura_Server
 
         private static void TestMethod()
         {
-            //Aura.Model.Purchase pur1 = new Aura.Model.Purchase()
-            //{
-            //    purchaseName = "Техобслуживание",
-            //    purchaseMethodID = 1,
-            //    purchacePrice = 50000f,
-            //    statusID = 0,
-            //    employeID = 1,
-            //    organizationID = 2,
 
-            //    bidsStartDate = new DateTime(2018, 8, 13).ToString(),
-            //    bidsEndDate = new DateTime(2018, 8, 20).ToString(),
-            //    bidsFinishDate = new DateTime(2018, 8, 23).ToString(),
 
-            //};
-
-            //Aura.Model.Purchase pur2 = new Aura.Model.Purchase()
-            //{
-            //    purchaseName = "Мягкий инвентарь",
-            //    purchaseMethodID = 2,
-            //    purchacePrice = 32504.54f,
-            //    statusID = 2,
-            //    employeID = 2,
-            //    organizationID = 1,
-
-            //    bidsStartDate = new DateTime(2018, 8, 15).ToString(),
-            //    bidsEndDate = new DateTime(2018, 8, 16).ToString(),
-            //    bidsFinishDate = new DateTime(2018, 8, 23).ToString(),
-            
-            //};
-
-            //Aura.Model.Purchase pur3 = new Aura.Model.Purchase()
-            //{
-            //    purchaseName = "Testing",
-            //    purchaseMethodID = 3,
-            //    statusID = 0,
-            //    employeID = 0,
-            //    organizationID = 3,
-
-            //    purchaseEisDate = new DateTime(2018, 8, 15).ToString(),
-                
-
-            //};
-
-            //Aura.Model.Purchase pur4 = new Aura.Model.Purchase()
-            //{
-
-            //};            
-
-            //Aura.Model.Purchase pur8 = new Aura.Model.Purchase()
-            //{
-
-            //};
-
-           
         }
 
 
         private static void LoadOrganisations()
         {
-            ExcelManagerImport excel = new ExcelManagerImport();
-            string fileName = "Журнал регистрации договоров 44-ФЗ 2018 год.xlsx";
-            string filePath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) + 
+            ListOfOrganistations listOfOrganisations44 = new ListOfOrganistations();
+            ListOfOrganistations listOfOrganisations223 = new ListOfOrganistations();
+
+            var table1 = LoadTable("Журнал регистрации договоров 44-ФЗ 2018 год.xlsx");
+            var table2 = LoadTable("Журнал регистрации договоров 223-ФЗ 2018 год.xlsx");
+
+            var table3 = LoadTable("Журнал регистрации 1 раз 44-фз 2018 год.xlsx");
+            var table4 = LoadTable("Журнал регистрации 1 раз 223-фз 2018 год.xlsx");
+
+            var table5 = LoadTable("Список предприятий c телефонами мупы11.xlsx");
+            var table6 = LoadTable("Список предприятий c тел. 223-ФЗ на 14.06.2018.xlsx");
+
+            int number = 0;
+            foreach (var str in table1)
+            {
+                
+                if (IsNotEmpty(str))
+                {
+                    Organisation org = new Organisation();
+                    org.name = str[0];
+
+                    //string regexPattern = "(.*)( от )";
+                    //string result = Regex.Match(str[1], regexPattern).Value.Replace(" от ","");
+                    //org.contractNumber = result;
+
+                    //regexPattern = ("( от )(.*)");
+                    //result = Regex.Match(str[1],regexPattern).Value.Replace(" от ", "");
+                    //org.contractStart = result;
+
+                    org.contractNumber = str[1];
+                    org.contractStart = str[3];
+                    try
+                    {
+                        org.contractEnd = str[4];
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(str.Count + " " + number);
+                    }
+
+                    switch (str[5])
+                    {
+                        case "оригинал": org.originalID = 1; break;
+                        case "оригинал в эл. виде": org.originalID = 3; break;
+                        default:
+                            {
+                                Console.WriteLine(org.name + " #" + str[5] + "#");
+                                org.originalID = Console.Read();
+
+                            }
+                            break;
+
+                    }
+
+                    org.inn = str[6];
+                    org.comments = str[7];
+
+                    org.contractCondition = 1;
+                    org.law = 1;
+                    org.contractType = 1;
+
+                    listOfOrganisations44.Add(org);
+                    number++;
+                }
+
+            }
+
+            foreach (var str in table2)
+            {
+                if (IsNotEmpty(str))
+                {
+                    Organisation org = new Organisation();
+
+                    org.name = str[0];
+                    org.contractNumber = str[1];
+                    org.contractStart = str[3];
+                    org.contractEnd = str[4];
+
+                    switch (str[5])
+                    {
+                        case "оригинал": org.originalID = 1; break;
+                        case "подписан ЭЦП": org.originalID = 3; break;
+                        case "нет оригинала": org.originalID = 2; break;
+                        default:
+                            {
+                                Console.WriteLine(org.name + " #" + str[5] + "#");
+                                org.originalID = Console.Read();
+
+                            }
+                            break;
+
+                    }
+
+                    org.inn = str[6];
+                    org.comments = str[7];
+
+                    org.contractCondition = 1;
+                    org.law = 2;
+                    org.contractType = 1;
+
+                    listOfOrganisations223.Add(org);
+
+                }
+            }
+
+            foreach (var str in table3)
+            {
+                if (IsNotEmpty(str))
+                {
+                    Organisation org = new Organisation();
+                    org.name = str[0];
+
+                    org.contractNumber = str[1];
+                    org.contractStart = str[3];
+                    org.contractEnd = str[4];
+
+                    switch (str[5])
+                    {
+                        case "оригинал": org.originalID = 1; break;
+                        case "оригинал в эл. виде": org.originalID = 3; break;
+                        default:
+                            {
+                                Console.WriteLine(org.name + " #" + str[5] + "#");
+                                org.originalID = Console.Read();
+
+                            }
+                            break;
+
+                    }
+
+                    org.inn = str[6];
+                    org.comments = str[7];
+
+                    org.contractCondition = 1;
+                    org.law = 1;
+                    org.contractType = 2;
+
+                    listOfOrganisations44.Add(org);
+
+                }
+            }
+
+            foreach (var str in table4)
+            {
+                if (IsNotEmpty(str))
+                {
+                    Organisation org = new Organisation();
+
+                    org.name = str[0];
+                    org.contractNumber = str[1];
+
+                    switch (str[3])
+                    {
+                        case "оригинал": org.originalID = 1; break;
+                        case "подписан ЭЦП": org.originalID = 3; break;
+                        case "нет оригинала": org.originalID = 2; break;
+                        default:
+                            {
+                                Console.WriteLine(org.name + " #" + str[5] + "#");
+                                org.originalID = Console.Read();
+
+                            }
+                            break;
+
+                    }
+
+                    org.inn = str[4];
+                    org.contactName = str[5];
+                   
+                    org.comments = str[6];
+
+                    org.contractCondition = 1;
+                    org.law = 2;
+                    org.contractType = 2;
+
+                    listOfOrganisations223.Add(org);
+
+                }
+            }
+
+            foreach (var str in table5)
+            {
+                if (IsNotEmpty(str))
+                {
+                    Organisation org = listOfOrganisations44.GetOrganisation(str[0]);
+                    if (org == null)
+                        org = listOfOrganisations44.GetOrganisation(str[1]);
+                    if (org == null)
+                    {
+                        org = new Organisation();
+                        listOfOrganisations44.Add(org);
+                    }
+
+                    if (org.name != str[0])
+                    {
+                        Console.WriteLine("#" + org.name + "# - #" + str[0] + "#");
+                        if (Console.Read() == 1)
+                            org.name = str[0];
+                    }
+
+                    if (org.inn == "")
+                        org.inn = str[1];
+                    else
+                    {
+                        if (org.inn != str[1])
+                            Console.WriteLine("#" + org.name + "# - #" + str[1] + "#");
+                        if (Console.Read() == 1)
+                            org.inn = str[1];
+                    }
+
+                    org.phoneNumber = str[2];
+                    org.contactName = str[3];
+                    org.email = str[4];
+
+
+                }
+            }
+
+            foreach (var str in table6)
+            {
+                if (IsNotEmpty(str))
+                {
+                    Organisation org = listOfOrganisations223.GetOrganisation(str[0]);
+                    if (org == null)
+                        org = listOfOrganisations223.GetOrganisation(str[1]);
+                    if (org == null)
+                    {
+                        org = new Organisation();
+                        listOfOrganisations223.Add(org);
+                    }
+
+                    if (org.name != str[0])
+                    {
+                        Console.WriteLine("#" + org.name + "# - #" + str[0] + "#");
+                        if (Console.Read() == 1)
+                            org.name = str[0];
+                    }
+
+                    if (org.inn == "")
+                        org.inn = str[1];
+                    else
+                    {
+                        if (org.inn != str[1])
+                            Console.WriteLine("#" + org.name + "# - #" + str[1] + "#");
+                        if (Console.Read() == 1)
+                            org.inn = str[1];
+                    }
+
+                    org.phoneNumber = str[2];
+                    org.contactName = str[3];
+                    org.email = str[4];
+                }
+            }
+
+
+            Console.WriteLine(listOfOrganisations44.Last().email);
+            Console.WriteLine(listOfOrganisations223.Last().email);
+        }
+
+        private static List<List<string>> LoadTable(string fileName)
+        {
+            string filePath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) +
                 "/" + fileName;
+            ExcelReader excel = new ExcelReader();
 
-            excel.LoadOrganisationsFromFile(filePath);
 
+            return excel.GetFromFile(filePath);
+        }
+
+        private static bool IsNotEmpty(List<string> list)
+        {
+            foreach (var str in list)
+            {
+                if (str != null && str != string.Empty)
+                    return true;
+            }
+
+            return false;
+        }
+
+    }
+
+    public class ListOfOrganistations : List<Organisation>
+    {
+        public bool ContainsInn(string inn)
+        {
+            foreach (var org in this)
+            {
+                if (org.inn == inn)
+                    return true;
+            }
+
+            return false;
+        }
+        
+        public bool ContainsName(string name)
+        {
+            foreach (var org in this)
+            {
+                if (org.name == name)
+                    return true;
+            }
+
+            return false;
+        }
+
+        public Organisation GetOrganisation(string nameOrInn)
+        {
+            foreach (var org in this)
+            {
+                if (org.name == nameOrInn)
+                    return org;
+
+                if (org.inn == nameOrInn)
+                    return org;
+            }
+
+            return null;
 
         }
+
 
     }
 
